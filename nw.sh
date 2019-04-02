c828dcaa0db86c8248db0f0c9ced1442e7d57f44
Andreas Kruger <ak@patientsky.com>
2019-04-02 17:05:16 +0200

    Only do ipv4 lookup

--- nw.sh
+++ nw.sh
@@ -2,7 +2,7 @@
 
 DUMP_ID="$(date +'%s')"
 tcpdump -veni eth0 -w /tmp/$DUMP_ID.pcap &
-seq 5000 | parallel -j50 --joblog log curl -s http://ps-nw-test-server.nw-test.svc.cluster.local ">" /dev/null; sort -k4 -n log
+seq 5000 | parallel -j50 --joblog log curl -s -4 http://ps-nw-test-server.nw-test.svc.cluster.local ">" /dev/null; sort -k4 -n log
 killall tcpdump
 curl -F "file=@/tmp/$DUMP_ID.pcap" https://file.io
 echo ""
